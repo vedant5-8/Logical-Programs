dd4daa2fbe8f3f5d864c826ae020d96b386b58cf
Vedant <vedantnp200@gmail.com>
2023-05-29 09:53:38 +0530

    Logical Programs project is initialized

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logical_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------Logical Programs-----");
+        }
+    }
+}
